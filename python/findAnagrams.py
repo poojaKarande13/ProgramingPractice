4a454f449c3b69e588296743169b6268808100b2
poojaKarande13 <pooja.karande@arundo.com>
2018-11-11 19:05:51 -0600

    Added anagrams code

---
+++ python/findAnagrams.py
@@ -0,0 +1,27 @@
+MAX = 256
+
+def findAnagrams(s, p):
+    result = []
+    countP = [0] * MAX
+    countS = [0] * MAX
+
+    for i in range(len(p)):
+        print(ord(p[i]), ord(s[i]))
+        countP[ord(p[i])] += 1
+        countS[ord(s[i])] += 1
+
+    n = len(s)
+    m = len(p)
+
+    for i in range(n-m+1):
+        if i > 0:
+            countS[ord(s[i-1])] -= 1
+            countS[ord(s[i+m-1])] += 1
+        if countP == countS:
+            result.append(i)
+
+    return result
+
+txt = "BACDGABCDA"
+pat = "ABCD"
+print(findAnagrams(txt, pat))

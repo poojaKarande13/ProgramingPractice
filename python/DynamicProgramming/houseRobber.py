7d918e6e4a8f7b7828e7ec3b69af360d7d42fc37
poojaKarande13 <poojapkarande@gmail.com>
2018-11-21 21:22:02 -0600

    Added leet code dynamic programming problem

---
+++ python/DynamicProgramming/houseRobber.py
@@ -0,0 +1,37 @@
+'''
+https://leetcode.com/problems/house-robber/
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+'''
+class Solution(object):
+    def rob(self, nums):
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+
+        res = [0  for i in range(n)]
+
+        res[0] = nums[0]
+        res[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            res[i] = max(res[i-1], nums[i] + res[i-2])
+
+        return res[n-1]

085ed855875a1cae8498e9231758c0d0b5c909f0
poojaKarande13 <poojapkarande@gmail.com>
2019-01-08 02:25:30 -0600

    Added computer science flashcards for interview preparation

--- Google/volumeOfLake.py
+++ Google/volumeOfLake.py
@@ -14,7 +14,7 @@ def volumeOfLake(arr):
     vol = 0
     # forward pass
     for i in range(N):
-        if arr[m] < arr[i]:
+        if arr[m] <= arr[i]:
             vol += calculateVol(arr, m, i)
             m = i
     m = N - 1
@@ -29,4 +29,4 @@ def volumeOfLake(arr):
 
     return vol
 #print(volumeOfLake([1,3,2,4,1,3,1,4,5,2,2,1,4,2,2]))
-print(volumeOfLake([1,2,3,4,5]))
+print(volumeOfLake([1,5,3,4,6,4,5,6]))
